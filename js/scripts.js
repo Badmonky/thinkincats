64c0cae54fd1e7c95cd3c58b774c4d99a8a78c11
Hermann, Sven (ext) (T RDA FOA ART-US) <sven.hermann.ext@siemens.com>
2022-01-23 12:13:31 -0700

    thinkin v1

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+/*
+   
+   Table Of Content
+   
+   1. Preloader
+   2. Smooth Scroll
+   3. Scroll Naviagation Background Change with Sticky Navigation
+   4. Mobile Navigation Hide or Collapse on Click 
+
+*/
+
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        
+       /* Preloader */
+		
+        $(window).load(function () {
+            $('.preloader').delay(800).fadeOut('slow');
+        });
+		
+		
+       /* Smooth Scroll */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+		
+
+
+       
+       /* Scroll Naviagation Background Change with Sticky Navigation */
+		 
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 100) {
+                $('.header-top-area').addClass('navigation-background');
+            } else {
+                $('.header-top-area').removeClass('navigation-background');
+            }
+        });
+
+		
+       /* Mobile Navigation Hide or Collapse on Click */
+		
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        
+		 });
+		
+
+
+        /* 3D Hover */
+        
+        $(".3d-hover").hover3d({
+           selector: ".hover-effect-3d",
+           sensitivity: 5
+        });
+		       
+		   
+        });
+
+   })(jQuery);
